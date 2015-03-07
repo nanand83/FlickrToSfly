b627b6ec8a178f85cdffb54677078fc01c2571e3
Anonymous <nanand83@gmail.com>
2015-03-07 15:14:47 +0000

    dbutils, flickclient code added

--- src/main/java/com/anand/client/FlickrClient.java
+++ src/main/java/com/anand/client/FlickrClient.java
@@ -1,4 +1,65 @@
 package com.anand.client;
 
+import com.anand.models.*;
+import com.anand.utils.*;
+
+import org.scribe.builder.*;
+import org.scribe.builder.api.*;
+import org.scribe.model.*;
+import org.scribe.oauth.*;
+
 public class FlickrClient {
+  	private String apiKey = "0fd992b0fff19e803daec389405acf52";
+  	private String apiSecret = "dd718bff9c133771";
+  
+  	private OAuthService service = null;
+  	private DBUtils dbUtils = null;
+  
+	public FlickrClient() {
+		service = new ServiceBuilder()
+                                .provider(FlickrApi.class)
+                                .apiKey(apiKey)
+                                .apiSecret(apiSecret)
+                                .build();
+		dbUtils = new DBUtils();     
+   }
+  
+  	public Token getTokenValue(String tokenType) {
+     	boolean doesTokenExist = dbUtils.doesTokenExist(tokenType);
+     	Token token = null;
+     	try {
+        if (doesTokenExist) {
+				token = dbUtils.getTokenValue(tokenType);
+        }  else {
+				System.out.println("Token of type "+tokenType+" does not exist in Db");
+          	System.exit(0);
+        }
+      } catch (Exception e) {
+        	System.out.println("Exception while retrieving token value from Db");
+        	e.printStackTrace();
+      }
+     
+     return token;
+   } 
+  
+  
+  	public String getResponseFromApi(String method) {
+     return "";
+   }
+  
+  	public Photosets getPhotosetsFromResponse(String response) {
+     return null;
+   }
+  
+  	public void printPhotoUrls(String albumName) {
+     
+     
+   }
+  
+  	public static void main(String args[]) {
+     FlickrClient client = new FlickrClient();
+     client.printPhotoUrls();
+   }
+  
 }
+
--- src/main/java/com/anand/utils/DBUtils.java
+++ src/main/java/com/anand/utils/DBUtils.java
@@ -1,4 +1,115 @@
 package com.anand.utils;
 
+import java.sql.*;
+import java.net.*;
+import java.io.*;
+
 public class DBUtils {
+  private static final String DATABASE_URL = "postgres://obsyetawdctbrh:KG6i6SyYHjXaPHYWQfNqkm2imb@ec2-54-204-39-187.compute-1.amazonaws.com:5432/dbcokog704adr7";
+  
+  public DBUtils() {
+    super();
+  }
+  
+  private static Connection getConnection() throws URISyntaxException, SQLException {
+    URI dbUri = new URI(DATABASE_URL);
+
+    String username = dbUri.getUserInfo().split(":")[0];
+    String password = dbUri.getUserInfo().split(":")[1];
+    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+    return DriverManager.getConnection(dbUrl, username, password);
+    
+  }
+  
+  public boolean doesTokenExist(String tokenType)  {
+    boolean doesTokenExist = false;
+    Connection conn = null;
+    ResultSet rs = null;
+    Statement stmt = null;
+    
+    try {
+    	conn = getConnection();
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("SELECT COUNT(*) FROM Tokens where type='"+tokenType+"'");
+      if (rs.next()) {
+        doesTokenExist = rs.getInt(1) == 1 ? true : false;
+      }
+    } catch (Exception e) {
+      System.out.println("Exception while checking if token exists in db or not");
+      e.printStackTrace();
+    } finally {
+      try {
+        if (rs != null) {
+           rs.close();
+        }
+        if (stmt != null) {
+           stmt.close();
+        }
+        if (conn != null) {
+           conn.close();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    
+    return doesTokenExist;
+  }
+  
+  public byte[] getTokenValueBytes(String tokenType) {
+    byte[] tokenValueBytes = null;
+    Connection conn = null;
+    ResultSet rs = null;
+    Statement stmt = null;
+    
+    try {
+    	conn = getConnection();
+      stmt = conn.createStatement();
+      rs = stmt.executeQuery("SELECT Value FROM Tokens where type='"+tokenType+"'");
+      if (rs.next()) {
+        tokenValueBytes = rs.getBytes(1);
+      }
+    } catch (Exception e) {
+      System.out.println("Exception while getting token value of type "+tokenType);
+      e.printStackTrace();
+    } finally {
+      try {
+        if (rs != null) {
+           rs.close();
+        }
+        if (stmt != null) {
+           stmt.close();
+        }
+        if (conn != null) {
+           conn.close();
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    
+    return tokenValueBytes;
+  }
+  
+  public Object getTokenValue(String tokenType) {
+    Object token = null;
+    
+    byte[] tokenBytes = getTokenValueBytes(tokenType);
+    if (tokenBytes!= null) {
+      try {
+      	token = deserialize(tokenBytes);
+      } catch (Exception e) {
+        	System.out.println("Exception while deserializing bytes");
+        	e.printStackTrace();
+      }
+    }
+    return token;
+  }
+  
+  public Object deserialize(byte[] inputBytes) throws IOException, ClassNotFoundException {
+    ByteArrayInputStream bais = new ByteArrayInputStream(inputBytes);
+    ObjectInputStream ois = new ObjectInputStream(bais);
+    return ois.readObject();
+  }
 }
